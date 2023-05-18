c046bbc0da6b448092ef213b6c3fa05b33c4b791
Nicol4sDev <alvareznicolasdev@gmail.com>
2023-05-18 20:42:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-slate-800 h-screen flex">
+    <QrComponent/>
+  </div>
+</template>
+
+<script setup>
+import QrComponent from './components/qr-component.vue';
+</script>
---
+++ src/components/qr-component.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col md:w-80 w-full h-auto md:h-[497px] border-white bg-white border-4 rounded-[20px] m-auto qr-shadow">
+        <div class="flex items-start justify-center m-4">
+            <img class="rounded-[10px] w-full h-auto" src="../assets/images/image-qr-code.png" alt="qr-image">
+        </div>
+        <div class="flex flex-col items-center justify-center w-72 md:w-72 mx-auto m-4">
+            <p class="text-[18px] md:text-[22px] text-[#1F314F] text-center font-bold leading-7">Improve your front-end skills by building projects</p>
+            <p class="text-[14px] md:text-[16px] text-[#7D889E] text-center font-normal leading-[18.9px] m-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+    </div>
+</template>
+
